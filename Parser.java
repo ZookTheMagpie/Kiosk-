aaccd789a570e0fc8c3466ea7b3597aa44d26512
HaIIvard <HaIIvard@158.38.193.211>
2016-03-04 11:31:31 +0100

    Added comments to the parser class.

--- Parser.java
+++ Parser.java
@@ -1,4 +1,3 @@
-package kioskprosjekt;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -18,23 +17,37 @@ public class Parser
 
     private CommandWords commands;
     private Scanner reader;
-
+/**
+ * Creates a CommandWords and Scanner object.
+ */
     public Parser()
     {
         commands = new CommandWords();
         reader = new Scanner(System.in);
     }
-
+/**
+ * 
+ * @return A refrence to the commandWords object.
+ */
     public CommandWords getCommandWords()
     {
         return commands;
     }
-
+/**
+ * promts the user to input.
+ * 
+ * @return The input from the user as a string.
+ */
     public String getInputString()
     {
         return reader.nextLine();
     }
-
+/**
+ * This method gets the user's input number and returns it.
+ * The input must be within a specified range.
+ * 
+ * @return The user's choice as an int.
+ */
     public int menuSelection()
     {
         System.out.println("> ");
@@ -45,7 +58,11 @@ public class Parser
         }
         return menuChoice;
     }
-    
+    /**
+     * Gets an input from the user and checks if it is 1 or 2.
+     * 
+     * @return The user's choice of 1 or 2 as an int.
+     */
         public int getYesOrNo()
     {
         System.out.println("> ");
@@ -57,7 +74,12 @@ public class Parser
         return YesOrNo;
     }
 
-
+/**
+ * This method converts a string input from the user to a 
+ * command of the command class.
+ * 
+ * @return The command the user submitted.
+ */
     public Command getCommand()
     {
         String inputLine;
