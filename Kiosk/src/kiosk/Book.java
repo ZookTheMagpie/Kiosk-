1fd3d7fb1586eab59718a7d1cd8f025b0b86f255
HaIIvard <HaIIvard@158.38.193.211>
2016-03-11 12:26:10 +0100

    Added litterature superclass

---
+++ Kiosk/src/kiosk/Book.java
@@ -0,0 +1,35 @@
+;
+
+/**
+ * This class contains the information that defines a book.
+ *
+ * @author HaIIvard
+ */
+public class Book extends Litterature
+{
+
+    private String author;
+
+    /**
+     *
+     * @param title the title of the book.
+     * @param author the author of the book
+     * @param publisher the publisher of the book
+     */
+    public Book(String title, String author, Publisher publisher)
+    {
+        super(title, publisher);
+        this.author = author;
+    }
+
+    /**
+     * Returns the author of the book.
+     *
+     * @return The author of the book.
+     */
+    public String getAuthor()
+    {
+        return author;
+    }
+
+}
