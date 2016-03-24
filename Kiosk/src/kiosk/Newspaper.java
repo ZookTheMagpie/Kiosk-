40e2caed40ad8a26aabb668a38a4f35c88b0beca
HaIIvard <HaIIvard@158.38.193.211>
2016-03-24 13:59:27 +0100

    Added missing comments to newspaper and Litterature class

--- Kiosk/src/kiosk/Newspaper.java
+++ Kiosk/src/kiosk/Newspaper.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
+ * This class contains the information that defines a newspaper.
  *
  * @author HaIIvard
  */
