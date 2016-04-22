8557ac462919b7c25b884c427c0c37a428245ec8
Øystein Barstad <oebars@gmail.com>
2016-04-22 13:51:18 +0200

    Bare bones version of the input boxes

---
+++ InputBox2.java
@@ -0,0 +1,143 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.text.Text;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Font;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.ToolBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.image.ImageView;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+
+/**
+ * Barebones version of the pop up boxes
+ * A modified version of Nested Containers example
+ * Author: Øystein Barstad
+ * Date: 22.04.2016
+ */
+
+/**
+ * Demonstrates the use of menus in JavaFX.
+ */
+public class InputBox2 extends Application
+{
+
+    /**
+     * The start-method is called by the JavaFX platform upon starting the
+     * JavaFX-platform. The method is abstract and must be overridden by any
+     * subclass of Application. The main window is setup in this method,
+     * including menus, toolboxes etc.
+     *
+     * @param primaryStage The main stage making up the main window.
+     */
+    @Override
+    public void start(Stage primaryStage)
+    {
+
+        BorderPane root = new BorderPane(); // Create the root node. The Menu will be placed at the top
+        
+
+        
+        // Place the StatusBar at the bottom
+        root.setBottom(createStatusBar());
+        // Place the centre content
+        root.setCenter(createCentreContent());
+
+        // Create the scene, adding the rootNode and setting the size
+        Scene scene = new Scene(root, 300, 250);
+        // Set title of the stage (window) and add the scene
+        primaryStage.setTitle("Complete GUI Example");
+        primaryStage.setScene(scene);
+        // Finally, make the stage (window) visible
+        primaryStage.show();
+    }
+
+    /**
+     * The stop() method is being called by the JavaFX-platform when the
+     * platform stops, are being terminated. This would typically happen as a
+     * result of the last open window being closed. Override this method to make
+     * sure that the application is terminated.
+     */
+    @Override
+    public void stop()
+    {
+        System.exit(0);
+    }
+
+    /**
+     * The entry-point to start the application.
+     *
+     * @param args Command-line arguments supplied during startup
+     */
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    /**
+     * Creates the content of the centre section of the GUI.
+     *
+     * @return the node to be placed in the centre of the BorderPane
+     */
+    private Node createCentreContent()
+    {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+       
+
+        Label title = new Label("Title:");
+        grid.add(title, 0, 1);
+
+        TextField titleField = new TextField();
+        grid.add(titleField, 1, 1);
+
+        Label publisher = new Label("Publisher:");
+        grid.add(publisher, 0, 2);
+
+        TextField publisherBox = new TextField();
+        grid.add(publisherBox, 1, 2);
+        
+        Label author = new Label("Author:");
+        grid.add(author, 0, 3);
+
+        TextField authorBox = new TextField();
+        grid.add(authorBox, 1, 3);
+
+        return grid;
+    }
+
+    /**
+     * Creates the StatusBar to be displayed at the bottom of the window.
+     *
+     * @return the StatusBar as a Node
+     */
+    private Node createStatusBar()
+    {
+        HBox statusBar = new HBox();
+        statusBar.setStyle("-fx-background-color: #999999;");
+        statusBar.getChildren().add(new Text("Status: OK"));
+
+        return statusBar;
+    }
+
+}
